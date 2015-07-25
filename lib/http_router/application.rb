0be2afb8f9ca52b7a7b2c92735fe8eaff8f82ecd
Tomasz Maczukin <tomasz@maczukin.pl>
2015-07-25 20:14:47 +0200

    Add #add_backend method to Application

--- lib/http_router/application.rb
+++ lib/http_router/application.rb
@@ -2,7 +2,7 @@ module HttpRouter
   # Application class
   #
   class Application
-    attr_reader :name, :ssl_required, :hostnames
+    attr_reader :name, :ssl_required, :hostnames, :backends
 
     def initialize(name)
       @name = name.to_s
@@ -15,6 +15,10 @@ module HttpRouter
 
     def add_hostname(hostname)
       @hostnames.push(hostname) unless @hostnames.include?(hostname)
+    end
+
+    def add_backend(backend)
+      @backends.push(backend) unless @backends.include?(backend)
     end
   end
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -54,6 +54,58 @@ module HttpRouter
       end
     end
 
+    context 'backends' do
+      before do
+        @unix_backend_1 = Backend::UnixSocket.new('/tmp/application_1.sock')
+        @unix_backend_2 = Backend::UnixSocket.new('/tmp/application_2.sock')
+
+        class Application
+          attr_writer :backends
+        end
+      end
+
+      subject do
+        lambda do
+          app = Application.new('example')
+          app.backends = [
+            @unix_backend_1,
+            @unix_backend_2
+          ]
+          app
+        end
+      end
+
+      describe '#add_backend' do
+        it 'should update backends array while adding new backend' do
+          unix_backend_3 = Backend::TCP.new('127.0.0.2', 8888)
+
+          app = subject.call
+          app.add_backend(unix_backend_3)
+
+          expect(app.backends).to include(unix_backend_3)
+          expect(app.backends.count).to eq(3)
+        end
+
+        it 'shouldn\'t modify backends array while adding existing backend' do
+          app = subject.call
+          app.add_backend(@unix_backend_1)
+
+          expect(app.backends).to include(@unix_backend_1)
+          expect(app.backends.count).to eq(2)
+        end
+      end
+
+      describe '#backends' do
+        it 'should return all added backends' do
+          app = subject.call
+
+          expect(app.backends[0]).to eq(@unix_backend_1)
+          expect(app.backends[1]).to eq(@unix_backend_2)
+          expect(app.backends.count).to eq(2)
+        end
+      end
+    end
+
     context 'requireing SSL' do
       it 'should have ability to mark application as "SSL required"' do
         app = Application.new('example')
